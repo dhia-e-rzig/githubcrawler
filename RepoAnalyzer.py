7b8e58e0a07198913a75e008650027aa05e623c7
dhiarzig <dhia.rzig@gmail.com>
2020-09-24 11:04:32 -0400

    enhanced the RepoAnalyzer.py repository analyser script and the special files data filetypes.csv, the outputs are now in output.csv for the special files and output_overall.txt for stats about the analysis

--- RepoAnalyzer.py
+++ RepoAnalyzer.py
@@ -1,3 +1,5 @@
+import math
+
 from git import Repo
 import pandas
 import os
@@ -10,52 +12,108 @@ import stat
 from pathlib import Path
 from os import walk
 
+progress = 0
+HDD = "F:"
 
-progress= 0
-HDD= "F:"
+repos_dir = HDD + "\\PhD Work\\repos\\"
 
-repos_dir=HDD+"\\PhD Work\\repos\\"
+output_file = open("output.csv", "wt+")
+output_file.write("Devops file?;Filepath;Devops Type \n")
 
-output_file = open("output.txt","a+")
-
-with open("analyzingAIMLprogress.json",'r+') as outfile:
-    progress=json.load(outfile)
-    print(progress)
+# with open("analyzingAIMLprogress.json", 'r+') as outfile:
+#     progress = json.load(outfile)
+#     print(progress)
 with open("filetypes.csv", "r", newline='') as files_list:
     special_files_dataframe = pandas.read_csv(files_list, delimiter=',')
 
+Directories = special_files_dataframe["Directory"].tolist()
+FileNamesAndExtensions = special_files_dataframe["FileNameAndExtension"].tolist()
+FileNamesAndExtensionsAndDirectories = list(zip(FileNamesAndExtensions, Directories))
+# special_files_dataframe.set_index("FileNameandExtension", inplace=True, drop=False)
+#
+# print(special_files_dataframe)
 
-Directories=special_files_dataframe["Directory"].tolist()
-FileNamesAndExtensions=special_files_dataframe["FileName"].tolist()
 
-special_files_dataframe.set_index("FileNameandExtension")
+mypath = path.join(HDD, repos_dir)
 
-mypath=path.join(HDD,repos_dir)
-
-count_dict={}
+count_dict = {}
 
 f = []
 
 count_dict["total"] = 0
 count_dict["unknown"] = 0
 for ext in FileNamesAndExtensions:
-    count_dict[ext]=0
+    count_dict[ext] = 0
 
+generic_ext = ['.csv', '.py', '.java', '.c', '.cpp', '.exe', '.cs', '.md', '.txt','.html','.ts','.go','.png','.js','.css','.s','.ini','.jpg','.bmp','ipynb','.map','.scss','.gif','.markdown','.pem','.sh','.xaml','.csproj','.onnx','.svg','.lock']
+ignorder_ext=['.gitignore','README.md','LICENSE',"AUTHORS","CONTRIBUTORS","PATENTS","OWNERS","SECURITY_CONTACTS","NOTICE","Readme",".DS_Store",".gitattributes","CODEOWNERS",".gitkeep",".gitmodules","GOLANG_CONTRIBUTORS"]
 
+for t1ext in generic_ext:
+    count_dict[t1ext] = 0
 
-def test(dirpath,filename):
-    count_dict["total"]+=1
-    for ext in FileNamesAndExtensions:
-        if filename==ext:
-            output_file.write(dirpath+" is implementing CI with a "+ special_files_dataframe[ext])
+for t2ext in ignorder_ext:
+    count_dict[t2ext] = 0
+
+def test(dirpath, filename):
+    filename = filename.strip()
+    count_dict["total"] += 1
+
+    if ".git" in dirpath:
+        return
+
+    if ".idea" in dirpath:
+        return
+    if ".vscode" in dirpath:
+        return
+    for iext in ignorder_ext:
+        if filename==iext:
+            count_dict[iext] += 1
+            return
+
+    for i in range(0, len(FileNamesAndExtensionsAndDirectories)):
+        (ext, dir) = FileNamesAndExtensionsAndDirectories[i]
+        # if  math.isnan(dir):
+        #     print(dir)
+        #     return
+        if isinstance(dir, str):
+            if dir !='NA' and dir not in dirpath:
+                continue
+
+        if filename == ext:
+            filetype = str(special_files_dataframe.loc[special_files_dataframe['FileNameAndExtension'] == ext]['FileType'].values).replace('[', '').replace(']', '').replace('\'','')
+            output_file.write("Yes"+';'+dirpath+ '\\' + filename +';'+ filetype + "\n")
             output_file.flush()
-            count_dict[ext]+=1
-        elif filename.endswith(ext):
-            count_dict[ext]+=1
-        else:
-            output_file.write(dirpath+'\\'+filename+" Unknown file")
-            count_dict["unknown"]+=1
+            count_dict[ext] += 1
+            return
+        elif filename.lower().endswith(ext):
+            filetype = str(special_files_dataframe.loc[special_files_dataframe['FileNameAndExtension'] == ext][
+                               'FileType'].values).replace('[', '').replace(']', '').replace('\'', '')
+            count_dict[ext] += 1
+            output_file.write("Unknown"+';'+dirpath + '\\' + filename +';'+ filetype +"\n")
+            output_file.flush()
+            return
 
-for (dirpath, dirnames, filenames) in walk(mypath):
+    for gext in generic_ext:
+        if filename.lower().endswith(gext):
+            count_dict[gext] += 1
+            return
+
+    count_dict["unknown"] += 1
+    output_file.write("Unknown"+';'+dirpath + '\\'+ filename + ';'+"Unknown file"+"\n")
+    output_file.flush()
+    return
+
+
+list_dirs = list(walk(mypath))
+
+for i in range(progress, len(list_dirs)):
+    (dirpath, dirnames, filenames) = list_dirs[i]
     for filename in filenames:
-        test(dirpath,filename)
+        test(dirpath, filename)
+    with open("analyzingAIMLprogress.json", 'w+') as outfile:
+        progresspickled = jsonpickle.encode(i, unpicklable=False)
+        outfile.write(progresspickled)
+
+
+with open("output_overall.txt", 'w+') as outfile:
+    outfile.write(str(count_dict) + "\n")
